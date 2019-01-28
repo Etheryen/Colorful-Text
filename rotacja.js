93cd11a932dec11c8240fd77c35aba20bb335e34
Etheryen <44708346+Etheryen@users.noreply.github.com>
2019-01-28 16:48:31 +0100

    Add files via upload

---
+++ rotacja.js
@@ -0,0 +1,40 @@
+var tekst = document.getElementById("tekst");
+
+function red() {
+    tekst.style.color = "red";
+    x = 1
+}
+
+function green() {
+    tekst.style.color = "lime";
+    x = 2
+}
+
+function blue() {
+    tekst.style.color = "blue";
+    x = 3
+}
+
+x = 3
+
+
+function loop() {
+    switch (x) {
+        case 3:
+            red();
+            break;
+        case 2:
+            blue();
+            break;
+        case 1:
+            green();
+            break;
+    }
+    setTimeout( loop, 75 );  
+}
+
+loop();
+
+
+
+
